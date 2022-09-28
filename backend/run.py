5d8649d957629ad49ea93eb49421beb04da3eeba
zhangjj <524266571@qq.com>
2022-09-28 17:18:57 +0800

    first commint

---
+++ backend/run.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, jsonify
+from random import *
+import os
+
+static_folder = os.path.join(os.path.abspath(os.path.dirname(os.getcwd())) + "\\dist\\static")
+template_folder = os.path.join(os.path.abspath(os.path.dirname(os.getcwd())) + "\\dist")
+app = Flask(__name__,
+            static_folder=static_folder,
+            template_folder=template_folder)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
